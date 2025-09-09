07e593b48c1d579091da499744956d52ce627210
JDev-il <jonathandanielwork@gmail.com>
2025-09-09 12:20:58 +0300

    main.ts config changes - 1. added setGlobalPrefix for 'api'; 2. added port '0.0.0.0' to enable global access (in prod)

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,22 @@ import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
-dotenv.config({ path: './.env' }); // Explicit path
+dotenv.config({ path: './.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.get(ConfigService);
+  app.setGlobalPrefix('api');
   app.enableCors({
-    // origin: 'http://localhost:4200',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-    // forbidNonWhitelisted: true, // Throw error if unexpected properties are found
     transform: true,
   }));
-  app.use(bodyParser.json()); // Parse incoming JSON payloads
-  app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded payloads
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
